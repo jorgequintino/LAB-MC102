d8a434142787c4b57c7814541bb85288394a4324
haitsr <jorgefelipe11_@hotmail.com>
2023-06-11 18:37:57 -0300

    aaaa

--- lab10.py
+++ lab10.py
@@ -1,4 +1,4 @@
-def damage(max_damage, cx, fx):
+def damage(max_damage, cx, fx, cy, fy):
     # same type of arrow
     if (cx - fx) < 0:
         x = (-1)*(cx - fx)
@@ -6,28 +6,50 @@ def damage(max_damage, cx, fx):
         x = (cx - fx)
     if (cy - fy) < 0:
         y = (-1)*(cy - fy)
-    if (cy - fy) >= 0:
+    elif (cy - fy) >= 0:
         y = (cy - fy)
     damage = max_damage - (x + y)
     # build for different types of arrows
     return damage
 
 
+def foco():
+    pass
+
+
+def living():
+    pass
+
+
+def attack():
+    pass
+
+
 def main():
-    aloy_max_lives = int(input())
+    max_lives = int(input())
     arrow_amount = [input(sep=" ")]
-    arrows  = {}
+    arrows = {}
     for i in arrow_amount:
         if i % 2 == 0:
-            arrows = {arrow_amount[i]: arrow_amount[i+1]}
+            arrows[arrow_amount[i]] = arrow_amount[i+1]
+    aloy = {max_lives, arrows}
     amount_monsters = int(input())
-    machines = int(input())
-    for _ in range(machines):
+    machines_combat = int(input())
+    machines = {}
+    for i in range(machines_combat):
         # lista é melhor
-        live_points, attack_points, parts_quant = input(sep=" ")
+        machine_details = input(sep=" ")
+        machines[i] = {}
+        machines[i][machine_details]
         # um dicionário {machines(número): [live_points, attack_points, parts_quant]}
-        for _ in range(parts_quant):
-            body_part, weaknees, max_damage, x_coordenate, y_coordenate = input(sep=", ")
+        for _ in range(machines[i][machine_details][2]):
+            body_part, weaknees, max_damage, x_coordenate, y_coordenate = input().split(sep=", ")
+            machines[i][machine_details][body_part] = [weaknees, max_damage, x_coordenate, y_coordenate]
             # parts {body_part: [weaknees, max_damage, (x_coordenate, y_coordenate)]}
-        target, body_part, arrow_type, fx, fy = input(sep=", ")
-    # confuso a parte final da entrada
+        target, body_part, arrow_type, fx, fy = input().split(sep=", ")
+        # chamar ataque aqui
+
+
+
+
+# Provavelmente vou ter que chamar os ataques na main dentro de cada for.
