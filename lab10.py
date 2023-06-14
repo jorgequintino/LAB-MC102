bfe6038973d7c45ca09030388132f279b92f94f4
haitsr <jorgefelipe11_@hotmail.com>
2023-06-13 23:18:18 -0300

    a

--- lab10.py
+++ lab10.py
@@ -147,9 +147,9 @@ def main():
                 if not aloy_alive:
                     combat = False
 
-            if aloy_alive:
-                aloy_life_points += max_life // 2
-                aloy_life_points = min(aloy_life_points, max_life)
+        if aloy_alive and combat is False:
+            aloy_life_points += max_life // 2
+            aloy_life_points = min(aloy_life_points, max_life)
 
         if not aloy_alive:
             print("Vida após o combate =", aloy_life_points)
