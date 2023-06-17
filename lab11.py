bdfbe78a8fa8db590ed0395a9309f9e31a57ce29
haitsr <jorgefelipe11_@hotmail.com>
2023-06-17 16:12:07 -0300

    link

--- lab11.py
+++ lab11.py
@@ -7,27 +7,56 @@ class Link:
         self.position = position
 
 
+    def walking():
+        pass
+        # primeiro ele desce até a última linha
+
+    
+    def combat():
+        pass
+        # it only happens once per position.
+
+    def collect_object():
+        pass
+    # Link must first collect the object in the position 
+    # he's in and then fight the existing monsters there.
+
+
 class Room:
-    def __init__ (self, line, column, exit):
-        self._line = line
-        self._column = column
+    def __init__ (self, lines, columns, exit):
+        self._lines = lines
+        self._columns = columns
         self._exit = exit
 
+    def create_room(self, stamp_room):
+        room = []
+        for _ in range(Room.lines):
+            line = []
+            for _ in range(Room.columns):
+                line.append(".")
+            room.append(line)
+        return stamp_room(room)
+   
+    def stamp_room (self, room):
+        for room_line in range(len(room)):
+            print(*room[room_line])
+        print()
+
     @property
-    def line(self):
-        return self._line
-
-    @line.setter
-    def line(self, line):
-        self._line = line
+    def lines(self):
+        return self._lines
+    
+    @lines.setter
+    def lines(self, lines):
+        self._lines = lines
 
     @property
-    def column(self):
-        return self._column
+    def columns(self):
+        return self._columns
 
-    @column.setter
-    def column(self, column):
-        self._column = column
+    @columns.setter
+    def column(self, columns):
+        self._columns = columns
 
     @property
     def exit(self):
@@ -37,21 +66,25 @@ class Room:
     def exit(self, exit):
         self._exit = exit
 
-    def stamp_room (self):
-        pass
-
 
 class monster:
     def __init__(self, monster_details):
         self.name = monster_details[]
-        self.life = monster_details[key][0]
+        self.life = monster_details[][0]
         self.damage = monster_details[][2]
 
+    def walking():
+        pass # if it wants to walk past the room, it must stay still.
+
+
+class object:
+    def __init__(self, object_details):
+        self.name = object_details.keys()
 
 
 def main():
     initial_life, initial_damage = input().split()
-    line, column = input().split()
+    lines, columns = input().split()
     position_link= input()
     exit_position = input()
     link = [int(initial_life), int(initial_damage), position_link, exit_position]
@@ -69,16 +102,22 @@ def main():
     for i in range(object_amount):
         o_name, o_type, o_position, o_status = input().split()
         object_details[o_name] = [o_type, o_position, o_status]
-    
-    f = Link(2, 10, (2, 2), (4, 5))
 
-    Room.stam_room()
+    # Link must attack first.
 
 
 
 
 
 
+
+
+
+
+
+f = Link(2, 10, (2, 2), (4, 5))
+
+Room.stam_room()
 
 if __name__ == "__main__":
     main()
