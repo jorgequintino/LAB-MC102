310ad31799591f2e699c07997201190bc98e5dac
haitsr <jorgefelipe11_@hotmail.com>
2023-06-17 17:26:57 -0300

    m

--- lab11.py
+++ lab11.py
@@ -1,22 +1,56 @@
 #  Link nas masmorras de Hyrule.
 
 class Link:
-    def __init__ (self, life, damage, position):
+    def __init__ (self, life, damage, position, alive):
         self.life = life
         self.damage = damage
         self.position = position
+        self.alive = alive
+
+    def walking_right(self, position):
+        return (position[0], position[1] + 1)
 
 
-    def walking():
-        pass
+    def walking_left(self, position):
+        return (position[0], position[1] - 1)
+
+
+    def walking_up(self, position):
+        return (position[0] + 1, position[1])
+    
+    
+    def walking_down(self, position):
+        return (position[0] - 1, position[1])
+
+
+    def walking(self, position, dungeon):
+        # call 'create_room'
+        if position[0] % 2 == 0:
+            if (position[1] + 1) < len(dungeon[0]):
+                position = (position[0], position[1] - 1)
+            elif (position[1]) == len(dungeon[0]) - 1 and position[0] != 0:
+                position = (position[0] + 1, position[1])
+        elif position[0] % 2 != 0:
+            if (position[1] - 1) >= 0:
+                position = (position[0], position[1] - 1)
+            elif position[1] == 0 and position[0] != 0:
+                position = (position[0] + 1, position[1])
+        return position
+
         # primeiro ele desce até a última linha
 
+    def living(self, life, alive):
+        if life == 0:
+            alive = False
+        elif life > 0:
+            alive = True
+        return alive
     
-    def combat():
+    def combat(self,):
         pass
         # it only happens once per position.
 
-    def collect_object():
+    def collect_object(self, ):
         pass
     # Link must first collect the object in the position 
     # he's in and then fight the existing monsters there.
@@ -28,12 +62,21 @@ class Room:
         self._columns = columns
         self._exit = exit
 
-    def create_room(self, stamp_room):
+    def create_room(self, stamp_room, link, exit):
         room = []
-        for _ in range(Room.lines):
+        for l in range(Room.lines):
             line = []
-            for _ in range(Room.columns):
-                line.append(".")
+            for c in range(Room.columns):
+                if (l, c) == link.position:
+                    line.append('P')
+                elif (l, c) == Room.exit:
+                    line.append('*')
+                # igual a posição do monstro
+                elif (l, c) == monster:
+                    # averiguar o tipo para imprimir a letra correspondente
+                    line.append('')
+                else:
+                    line.append('.')
             room.append(line)
         return stamp_room(room)
    
@@ -67,7 +110,7 @@ class Room:
         self._exit = exit
 
 
-class monster:
+class Monster:
     def __init__(self, monster_details):
         self.name = monster_details[]
         self.life = monster_details[][0]
@@ -85,10 +128,12 @@ class object:
 def main():
     initial_life, initial_damage = input().split()
     lines, columns = input().split()
-    position_link= input()
+    link_position= input()
     exit_position = input()
-    link = [int(initial_life), int(initial_damage), position_link, exit_position]
-    # link = [vida inicial, dano inicial, posição, saída]
+    link_alive = True
+    link = Link(int(initial_life), int(initial_damage), link_position, link_alive)  # link = [vida inicial, dano inicial, posição]
+    dungeon = Room(lines, columns, exit_position)
+    
 
     # monster = []
     monster_details = {}  # {monstro: [vida, ataque, tipo, posição]}
@@ -96,26 +141,28 @@ def main():
     for i in range(monster_amount):
         m_life, m_attack, m_type, m_position = input().split()
         monster_details[i] = [int(m_life), int(m_attack), m_type, m_position]
+        # perhaps save it by the position, it could help get details better when needed.
 
     object_details = {}  # {nome: [tipo, posição, status]}
     object_amount = int(input())
     for i in range(object_amount):
         o_name, o_type, o_position, o_status = input().split()
         object_details[o_name] = [o_type, o_position, o_status]
+        # salvar por posição, deve ser mais fácil checar
 
+    # I DONT KNOW HOW TO EXACTLY CALL THE SHOTS HERE
     # Link must attack first.
+    # Link must walk first than the monsters
+
+
+    
 
 
 
 
 
 
-
-
-
-
-
-f = Link(2, 10, (2, 2), (4, 5))
+# f = Link(2, 10, (2, 2), (4, 5))
 
 Room.stam_room()
 
