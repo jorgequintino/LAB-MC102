9a7d3ae2bacd65ff5a7839ec63385fc870ff1a87
haitsr <jorgefelipe11_@hotmail.com>
2023-06-19 18:00:20 -0300

    almost

--- lab11.py
+++ lab11.py
@@ -14,25 +14,22 @@ class Link:
         self._position = (self._position[0], self._position[1] - 1)
 
     def walking_up(self):
-        self._position = (self._position[0] + 1, self._position[1])
+        self._position = (self._position[0] - 1, self._position[1])
 
     def walking_down(self):
-        self._position = (self._position[0] - 1, self._position[1])
+        self._position = (self._position[0] + 1, self._position[1])
 
     def walking(self, dungeon):
         if self._position[0] % 2 == 0:
-            if (self._position[1] + 1) < len(dungeon[0]):
-                self._position = (self._position[0], self._position[1] - 1)
-            elif (self._position[1]) == len(dungeon[0]) - 1 and self._position[0] != 0:
-                self._position = (self._position[0] + 1, self._position[1])
-        elif self._position[0] % 2 != 0:
             if (self._position[1] - 1) >= 0:
-                self._position = (self._position[0], self._position[1] - 1)
+                self.walking_left()
             elif self._position[1] == 0 and self._position[0] != 0:
-                self._position = (self._position[0] + 1, self._position[1])
-        return self._position
-
-        # primeiro ele desce até a última linha
+                self.walking_up()
+        elif self._position[0] % 2 != 0:
+            if (self._position[1] + 1) < dungeon.columns:
+                self.walking_right()
+            elif (self._position[1]) == dungeon.columns - 1 and self._position[0] != 0:
+                self.walking_up()
 
     def living(self):
         if self._life == 0:
@@ -42,40 +39,78 @@ class Link:
         return self._alive
 
     def collect_object(self, objects):
-        for object in objects:
+        used_objects = []
+        for index, object in enumerate(objects):
             if self._position == object._position:
                 if object._type == "v":
                     self._life += object._status
                     self._life = max(0, self._life)
-                    self._alive = self.living(self)
-                    if not self._alive:
-                        # print()
-                        # do the things of a dead body
+                    self._alive = self.living()
+                    used_objects.append(object)
+                    print("[v] Personagem adquiriu o objeto ", object._name, " com status de ", object._status, sep='')
                 elif object._type == "d":
                     self._damage += object._status
                     self._damage = max(1, self._damage)
-                    print([d]Personagem adquiriu o objeto joia com status de 2)
-    # Link must first collect the object in the position
-    # he's in and then fight the existing monsters there.
-    
-    def combat(self, monsters):
-            dead_monsters = []
-            for index, monster in enumerate(monsters):
-                if self._position == monster._position:
-                    monster._life -= self._damage
-                    monster._alive = monster.living(monster)
-                    if monster._alive:
-                        self._life -= monster._attack
-                        self._life = max(0, self._life)
-                        self._alive = self.living(self)
-                    elif not monster._alive:
-                        dead_monsters.append(index)
-                if not self._alive:
-                    # fazer as coisas da morte de P
-                    break
-            for dead_index in dead_monsters:
-                del monster[dead_index]
-            # it only happens once per position.
+                    used_objects.append(object)
+                    print("[d] Personagem adquiriu o objeto ", object._name, " com status de ", object._status, sep='')
+
+        for object in used_objects:
+            objects.remove(object)
+        return objects, self.alive
+
+    def combat(self, monsters, link, objects, dungeon):
+        dead_monsters = []
+        for index, monster in enumerate(monsters):
+            if self._position == monster._position:
+                monster._life -= self._damage
+                print("O Personagem deu ", self._damage, " de dano ao monstro na posicao ", self._position, sep='')
+                monster._alive = monster.living()
+                if monster._alive:
+                    self._life -= monster._attack
+                    self._life = max(0, self._life)
+                    print("O Monstro deu ", monster._attack, " de dano ao Personagem. Vida restante = ", self._life, sep='')
+                    self._alive = self.living()
+                elif not monster._alive:
+                    dead_monsters.append(monster)
+            # if not self._alive:
+            #     dungeon.create_room(dungeon.stamp_room, monster, link, objects)
+            #     break
+        for monster in dead_monsters:
+            monsters.remove(monster)
+        return monsters, self.alive
+        #  it only happens once per position.
+
+    @property
+    def life(self):
+        return self._life
+
+    @life.setter
+    def life(self, life):
+        self._life = life
+
+    @property
+    def damage(self):
+        return self._damage
+
+    @damage.setter
+    def damage(self, damage):
+        self._damage = damage
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, position):
+        self._position = position
+
+    @property
+    def alive(self):
+        return self._alive
+
+    @alive.setter
+    def alive(self, alive):
+        self._alive = alive
 
 class Room:
     def __init__(self, lines, columns, exit):
@@ -83,32 +118,37 @@ class Room:
         self._columns = columns
         self._exit = exit
 
-    def create_room(self, stamp_room, link, monster_details, object_details):
+    def create_room(self, stamp_room, monster_details, link, object_details):
+        end = False
         room = []
-        for L in range(self._lines):
+        for _ in range(self._lines):
             line = []
-            for C in range(self._columns):
+            for _ in range(self._columns):
                 line.append('.')
             room.append(line)
 
         created = False
         while not created:
-                
+
             for o in object_details:
                 room[o._position[0]][o._position[1]] = o._type
 
             for m in monster_details:
-                room[m._position[0]][m._position[1]] = m._type
+                room[m._position[0]][m._position[1]] = m.type
 
             room[self._exit[0]][self._exit[1]] = "*"
 
             if link._alive:
                 room[link._position[0]][link._position[1]] = "P"
+                if link.position == self.exit:
+                    end = True
                 created = True
             elif not link._alive:
                 room[link._position[0]][link._position[1]] = "X"
+                end = True
                 created = True
         stamp_room(room)
+        return end
 
     def stamp_room(self, room):
         for room_line in range(len(room)):
@@ -133,7 +173,7 @@ class Room:
 
     @property
     def exit(self):
-        return self.exit
+        return self._exit
 
     @exit.setter
     def exit(self, exit):
@@ -155,25 +195,24 @@ class Monster:
         self._position = (self._position[0], self._position[1] - 1)
 
     def walking_up(self):
-        self._position = (self._position[0] + 1, self._position[1])
+        self._position = (self._position[0] - 1, self._position[1])
 
     def walking_down(self):
-        self._position = (self._position[0] - 1, self._position[1])
+        self._position = (self._position[0] + 1, self._position[1])
 
     def walking(self, dungeon):
         if self._type == "U":
             if self._position[0] != 0:
-                Monster.walking_up(self)
-        if self._type == "D":
-            if self._position[0] != dungeon._lines - 1:  # problema
-                Monster.walking_down(self)
-        if self._type == "L":
+                self.walking_up()
+        elif self._type == "D":
+            if self._position[0] != dungeon._lines - 1:
+                self.walking_down()
+        elif self._type == "L":
             if self._position[1] != 0:
-                Monster.walking_left(self)
-        if self._type == "R":
+                self.walking_left()
+        elif self._type == "R":
             if self._position[1] != dungeon._columns - 1:
-                Monster.walking_left(self)
-        return self._position
+                self.walking_right()
 
     def living(self):
         if self._life == 0:
@@ -182,13 +221,68 @@ class Monster:
             self._alive = True
         return self._alive
 
+    @property
+    def life(self):
+        return self._life
+
+    @life.setter
+    def life(self, life):
+        self._life = life
+
+    @property
+    def attack(self):
+        return self._attack
+
+    @attack.setter
+    def attack(self, attack):
+        self._attack = attack
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        self._type = type
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, position):
+        self._position = position
+
+    @property
+    def alive(self):
+        return self._alive
+
+    @alive.setter
+    def alive(self, alive):
+        self._alive = alive
 
 class Object:
-    def __init__(self, o_name, o_type, o_position, o_status):
-        self._name = o_name
-        self._type = o_type
-        self._position = o_position
-        self._status = o_status
+    def __init__(self, name, type, position, status):
+        self._name = name
+        self._type = type
+        self._position = position
+        self._status = status
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def position(self):
+        return self._position
+
+    @property
+    def status(self):
+        return self._status
 
 def main():
     initial_life, initial_damage = input().split()
@@ -213,46 +307,32 @@ def main():
 
     link_alive = True
     link = Link(int(initial_life), int(initial_damage), link_position, link_alive)
-    dungeon = Room(lines, columns, exit_position)
+    dungeon = Room(int(lines), int(columns), exit_position)
     beginning = True
-    
-    while True:
+    end = False
+
+    end = dungeon.create_room(dungeon.stamp_room, monster_details, link, object_details)
+    while not end:
+
         while beginning:
+
             if link._position[0] != dungeon._lines - 1:
-                link.walking_down
-                for m in monster_details:
-                    m.walking
+                link.walking_down()
+                for monster in monster_details:
+                    monster.walking(dungeon)
+                object_details, link_alive = link.collect_object(object_details)
+                monster_details, link_alive = link.combat(monster_details, link, object_details, dungeon)
+                end = dungeon.create_room(dungeon.stamp_room, monster_details, link, object_details)
             elif link._position[0] == dungeon._lines - 1:
                 beginning = False
 
+        link.walking(dungeon)
+        for monster in monster_details:
+            monster.walking(dungeon)
+        object_details, link_alive = link.collect_object(object_details)
+        monster_details, link_alive = link.combat(monster_details, link, object_details, dungeon)
+        end = dungeon.create_room(dungeon.stamp_room, monster_details, link, object_details)
 
-        link.combat(monster_details)
-        link.collect_object(object_details)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# I DONT KNOW HOW TO EXACTLY CALL THE SHOTS HERE
-# Link must attack first.
-# Link must walk first than the monsters
-
-# f = Link(2, 10, (2, 2), (4, 5))
-
-# Room.stam_room()
 
 if __name__ == "__main__":
     main()
